42ed36d684e40a17bf81172f9e4dd15a750acfee
Jetmir <jetmir.hysi@inovalab.dev>
2022-11-24 17:03:30 +0100

    Added first API fetch call

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,23 @@
+async function getLatestCryptocurrencyListings(url) {
+    // Default options are marked with *
+
+    const response = await fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CMC_PRO_API_KEY':'42aca7ab-c1ba-4b97-8ede-d6a04d97da26'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      });
+
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  getLatestCryptocurrencyListings('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest')
+    .then((response) => {
+        console.log(response);
+    })
