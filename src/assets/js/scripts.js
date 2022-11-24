de7a32c82604beb2e70b4abb556fcf3cf5f4c6fa
Jetmir <jetmir.hysi@inovalab.dev>
2022-11-24 17:31:09 +0100

    Added credentials to API

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -3,14 +3,12 @@ async function getLatestCryptocurrencyListings(url) {
 
     const response = await fetch(url, {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
+        mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'omit', // include, *same-origin, omit
+        credentials: 'include', // include, *same-origin, omit
         headers: {
           'Content-Type': 'application/json',
-          'X-CMC_PRO_API_KEY':'42aca7ab-c1ba-4b97-8ede-d6a04d97da26',
-          'Access-Control-Request-Method': 'GET',
-          'Access-Control-Allow-Headers': 'Content-Type, X-CMC_PRO_API_KEY'
+          'X-CMC_PRO_API_KEY':'42aca7ab-c1ba-4b97-8ede-d6a04d97da26'
         },
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
